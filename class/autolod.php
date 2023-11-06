6e620b99822a7e2440f954e43e39956b77f72c4b
ali <ali.naseri.php@gmail.com>
2023-11-06 10:08:56 +0330

    fiks_bug

--- class/autolod.php
+++ class/autolod.php
@@ -1,7 +1,10 @@
 <?php
 function autolod($class)
-{$class_file= __DIR__."/$class.php";
-    include $class_file;
-
+{
+    $class_file = __DIR__ . "/$class.php";
+    if (file_exists($class_file)) {
+        include $class_file;
+    }
 }
+
 spl_autoload_register("autolod");
